788e448bc786b4e0d667e0c3c542673ed7b0500b
Lukasz Michalski <lm@zork.pl>
2020-11-07 22:36:39 +0100

    fixed package removal for yay dirs

--- clear_cache.sh
+++ clear_cache.sh
@@ -12,22 +12,34 @@ WORKING_DIR=$(pwd)
 
 function clear_cache_dir {
     declare -A packages=()
-    echo "Creating package list in $1"
-    for filepath in $1/*.pkg.*; do
+    if [ "$2" != "quiet" ]; then
+        echo "Creating package list in $1"
+    fi
+    pattern="$1/*.pkg.*"
+    for filepath in ${pattern}; do
+        if [ "${filepath}" == "${pattern}" ]; then
+            # no files found
+            continue;
+        fi;
         filename=$(basename {$filepath})
+        # get package name witohut version and add it to array
         package_name=$(echo ${filename} | sed -n 's/^\(.*\)-.*-[0-9]\+-\(x86_64\|any\)\.pkg\..*$/\1/p')
         if [ ! -z ${package_name} ]; then
             packages[${package_name}]=1
         else
             echo "WARNING: failed to parse package name ${package_name}, ignoring"
         fi
-        #exit 1;
     done;
 
-    echo "Removing old files for ${#packages[@]} package(s) from $1"
+    # got package list to remove, find old files and remove them all
+    if [ "$2" != "quiet" ]; then
+        echo "Removing old files for ${#packages[@]} package(s) from $1"
+    fi;
     for pkg in "${!packages[@]}"; do
         pkgpattern="$1/${pkg}*"
-        pkgfiles=$(ls -t ${pkgpattern})
+        # do not touch other files that do not 
+        # follow package naming pattern
+        pkgfiles=$(ls -t ${pkgpattern} |grep -E '^.*-.*-[0-9]+-(x86_64|any)\.pkg\..*$')
         remove=0
         for pkgpath in ${pkgfiles}; do
             if [ ${remove} -eq 1 ]; then
@@ -42,9 +54,12 @@ function clear_cache_dir {
 }
 
 
-clear_cache_dir ${PACMAN_CACHE}
+#clear_cache_dir ${PACMAN_CACHE}
 if [ ! -z  $1 ]; then
-    clear_cache_dir $1
+    echo "Cleaning yay cache dir $1"
+    for dir in $(ls -d $1/*/); do
+        clear_cache_dir ${dir} quiet
+    done;
 fi
 
 
